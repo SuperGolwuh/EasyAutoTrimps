b0ad4f74365dcc3201247d84c3af0d968bd051f0
SuperGolwuh <50163086+SuperGolwuh@users.noreply.github.com>
2019-05-01 00:38:13 -0700

    Update EasyAutoTrimps.js

--- EasyAutoTrimps.js
+++ EasyAutoTrimps.js
@@ -16,7 +16,6 @@ script.id = 'EasyAutoTrimps-GW-GolwuhPatch';
 //This can be edited to point to your own Github Repository URL.
 script.src = 'https://github.com/SuperGolwuh/EasyAutoTrimps/EasyAutoTrimps.js';
 //script.setAttribute('crossorigin',"use-credentials");
-script.setAttribute('crossorigin',"anonymous");
 document.head.appendChild(script);
 var ATversion = '2.1.7.1'; //when this increases it forces users setting update to newer version format
 
