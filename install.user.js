311ef0b50012a43ca43b99c4acdd3941731d052b
SuperGolwuh <50163086+SuperGolwuh@users.noreply.github.com>
2019-05-01 00:23:24 -0700

    Update install.user.js

--- install.user.js
+++ install.user.js
@@ -2,12 +2,12 @@
 // @name         AutoTrimps-EAT-SuperGolwuh
 // @version      0.1337
 // @namespace    https://github.com/SuperGolwuh/EasyAutoTrimps/
-// @updateURL    https://github.com/SuperGolwuh/EasyAutoTrimps/AutoTrimps2/.user.js
+// @updateURL    https://github.com/SuperGolwuh/EasyAutoTrimps/install.user.js
 // @description  Automate all the trimps!
 // @author       zininzinin, spindrjr, Ishkaru, genBTC, Zeker0, Meowchan, SuperGolwuh
 // @include      *trimps.github.io*
-// @include      *kongregate.com/games/GreenSatellite/trimps*
-// @connect      *https://github.com/SuperGolwuh/EasyAutoTrimps*
+// @include      *kongregate.com/games/GreenSatellite/trimps
+// @connect      *https://github.com/SuperGolwuh/EasyAutoTrimps
 // @connect      *trimps.github.io*
 // @connect      self
 // @grant        none
@@ -16,7 +16,7 @@
 var script = document.createElement('script');
 script.id = 'EasyAutoTrimps-GW-GolwuhPatch';
 //This can be edited to point to your own Github Repository URL.
-script.src = 'https://github.com/SuperGolwuh/EasyAutoTrimps/AutoTrimps2.js';
+script.src = 'https://github.com/SuperGolwuh/EasyAutoTrimps/EasyAutoTrimps.js';
 //script.setAttribute('crossorigin',"use-credentials");
 script.setAttribute('crossorigin',"anonymous");
 document.head.appendChild(script);
